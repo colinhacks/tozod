15f5651b7767ca703a58f25aae486ef15dcb7305
Luke Zehrung <lzehrung@gmail.com>
2023-03-29 09:43:35 -0400

    inline type

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import * as z from 'zod';
 
-type Matches<T> = {
-  [K in keyof T]: z.ZodType<T[K], any>
-};
-
-type ZodMatches<T> = z.ZodObject<Matches<T>> & z.ZodSchema<T>;
-
-export type toZod<T> = ZodMatches<T>;
+export type toZod<T> = z.ZodObject<{ [K in keyof T]: z.ZodType<T[K], any> }> & z.ZodSchema<T>;
