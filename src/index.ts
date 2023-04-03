963f9c443ea80adf7fe46e850ea5c07d8090512d
Luke Zehrung <lzehrung@gmail.com>
2023-04-03 13:04:18 -0400

    Revert "update to handle ZodEffects"
    This reverts commit 3a4639da9edeb61dd23d0de3993e45ce71daa964.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 import * as z from 'zod';
 
-type Schema<T> = z.ZodObject<{ [K in keyof T]: z.ZodType<T[K], any> }> & z.ZodSchema<T>;
-type Effects<T> = z.ZodEffects<Schema<T>>;
-
-export type toZod<T> = Schema<T> | Effects<T>;
+export type toZod<T> = z.ZodObject<{ [K in keyof T]: z.ZodType<T[K], any> }> & z.ZodSchema<T>;
